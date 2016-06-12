16cb98f4e43ddf6af0c84b010765d47bffb2da59
Brian Sarbin <sarbinb@gmail.com>
2016-06-12 09:36:54 -0700

    Adding in scaffolding for the project pages.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,34 @@
+//------------------------------------------------------------
+// Section to load all of the correct detail pages.
+//------------------------------------------------------------
 $("#iwan-ivpn-project").click(function () {
 	$(".details-page").load("pages/iwan-ivpn-project.html");
+});
+
+$("#vbranch-project").click(function () {
+	$(".details-page").load("pages/vbranch-project.html");
+});
+
+$("#cloudvpn-project").click(function () {
+	$(".details-page").load("pages/cloudvpn-project.html");
+});
+
+$("#oban-project").click(function () {
+	$(".details-page").load("pages/oban-project.html");
+});
+
+$("#clife-project").click(function () {
+	$(".details-page").load("pages/clife-project.html");
+});
+
+$("#elife-project").click(function () {
+	$(".details-page").load("pages/elife-project.html");
+});
+
+$("#olympics-project").click(function () {
+	$(".details-page").load("pages/olympics-project.html");
+});
+
+$("#mobile-project").click(function () {
+	$(".details-page").load("pages/mobile-project.html");
 });
