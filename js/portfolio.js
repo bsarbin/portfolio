f15a1878a75a4515ecac13e96b943588837c5c68
Brian Sarbin <sarbinb@gmail.com>
2016-06-20 01:40:19 -0700

    Major revisions and an overhaul of the navigational elements. TODO: - Review content - Fix navigation header for phone-size screens

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,10 @@
 //------------------------------------------------------------
 // Section to load all of the correct detail pages.
 //------------------------------------------------------------
+$(document).on('click','.home-link',function () {
+	loadPage(this, 'home');
+});
+
 $(document).on('click','.my-process-link',function () {
 	loadPage(this, 'my-process');
 });
@@ -41,51 +45,82 @@ $(document).on('click','.mobile-project',function () {
 	loadPage(this, "mobile-project");
 });
 
-function loadPage(element, url, isBrowserBack) {
-	var activeLink = '.' + url;
+$(document).on('click', '.about', function() {
+	$('.about-page').addClass('open-about');
+	$('.about-overlay').removeClass('hidden');
+})
 
-	if (url == null) {
-		showContent();
-		$('.my-process').addClass('active-link');
+$(document).on('click', '.about-overlay', function() {
+	$('.about-page').removeClass('open-about');
+	$('.about-overlay').addClass('hidden');
+});
+
+$(document).on('click','.right-arrow',function() {
+		 if ($('.details').hasClass('iwan-ivpn-page')) { loadPage(this,'vbranch-project'); }
+	else if ($('.details').hasClass('vbranch-page')) { loadPage(this,'cloudvpn-project'); }
+	else if ($('.details').hasClass('cloudvpn-page')) { loadPage(this,'clife-project'); }
+	else if ($('.details').hasClass('clife-page')) { loadPage(this,'elife-project'); }
+	else if ($('.details').hasClass('elife-page')) { loadPage(this,'olympics-project'); }
+	else if ($('.details').hasClass('olympics-page')) { loadPage(this,'mobile-project'); }
+});
+
+$(document).on('click','.left-arrow',function() {
+		 if ($('.details').hasClass('mobile-page')) { loadPage(this,'olympics-project'); }
+	else if ($('.details').hasClass('olympics-page')) { loadPage(this,'elife-project'); }
+	else if ($('.details').hasClass('elife-page')) { loadPage(this,'clife-project'); }
+	else if ($('.details').hasClass('clife-page')) { loadPage(this,'cloudvpn-project'); }
+	else if ($('.details').hasClass('cloudvpn-page')) { loadPage(this,'vbranch-project'); }
+	else if ($('.details').hasClass('vbranch-page')) { loadPage(this,'iwan-ivpn-project'); }
+});
+
+function loadPage(element, url, isBrowserBack) {
+	if (!isBrowserBack) {
+		window.history.pushState(url, null, null);
 		$(window).scrollTop(0);
-		activeLink = '.my-process-link';
+	}
+
+	var activeLink = '.' + url;
+	if (url == null || url == 'my-work') {
+		showContent();
+		$('.my-work').addClass('active-link');
+		activeLink = '.my-work-link';
+		$(window).scrollTop($('.my-work').offset().top);
+	}
+	else if (url == "home") {
+		showContent();
+		activeLink = '.home-link';
+		url = 'home-link';
+		$(window.scrollTop(0));
 	}
 	else if (url == 'my-process') {
 		showContent();
-		$(window).scrollTop($(".my-process").position().top);
 		activeLink = '.my-process-link';
-	}
-	else if (url == 'my-work') {
-		showContent();
-		$(window).scrollTop($(".my-work").position().top);
-		activeLink = '.my-work-link';
+		url = 'my-process-link';
+		$(window).scrollTop($('.my-process').offset().top);
 	}
 	else {
 		$(".details-page").load('pages/' + url + '.html');
 		hideContent();
-		$(window).scrollTop(0);
 	}
+
+	if (url == 'iwan-ivpn-project') { $('.left-arrow').addClass('disabled'); $('.right-arrow').removeClass('disabled'); }
+	else if (url == 'mobile-project') { $('.right-arrow').addClass('disabled'); $('.left-arrow').removeClass('disabled'); }
+	else { $('.left-arrow').removeClass('disabled'); $('.right-arrow').removeClass('disabled'); }
 
 	$('.active-link').removeClass('active-link');
-	$(activeLink).addClass('active-link');
-
-	if (!isBrowserBack) {
-		history.pushState(url, null, null);
-	}
-
-	if ($(window).width() < 900) {
-		hideMenu();
-	}
+	//$(activeLink).addClass('active-link');	
 }
 
 function hideContent() {
-	$('.main-content').addClass('hidden');
-	$('.details-page').removeClass('hidden');
+	//$('.main-content').addClass('hidden');
+	//$('.details-page').removeClass('hidden');
+	$('.top-nav').addClass('project-page');
 }
 
 function showContent() {
-	$('.main-content').removeClass('hidden');
-	$('.details-page').addClass('hidden');
+	//$('.main-content').removeClass('hidden');
+	//$('.details-page').addClass('hidden');
+	$('.top-nav').removeClass('project-page');
 }
 
 function hideMenu() {
@@ -93,6 +128,7 @@ function hideMenu() {
 	$('.nav').removeClass('animate-right');
 	$('.overlay-layer').addClass('animate-fade-out');
 	$('.nav').addClass('animate-left');
+	$('.hamburger').removeClass('is-active');
 }
 
 function showMenu() {
@@ -100,11 +136,13 @@ function showMenu() {
 	$('.nav').removeClass('animate-left');
 	$('.overlay-layer').addClass('animate-fade-in');
 	$('.nav').addClass('animate-right');
+	$('.hamburger').addClass('is-active');
 }
 
-window.addEventListener('popstate', function(e){
+window.onpopstate = function(e) {
+	console.log('e: ' + e.state);
 	loadPage(null, e.state, true);
-});
+};
 
 
 //Buttons to show and hide the navigation in small window mode
@@ -113,5 +151,44 @@ $(document).on('click', '.overlay-layer', function () {
 });
 
 $(document).on('click', '.show-menu', function () {
-	showMenu();
+	if ($('.show-menu').hasClass('is-active')) 	{ hideMenu(); }
+	else 										{ showMenu(); }
+});
+
+//Scroll events for the top-nav
+var lastScrollTop = 0;
+
+//Set the header color based on the initial scroll position
+$(document).ready(function() {
+	//Color the header if it's on the main title
+   if (!($('.top-nav').hasClass('project-page')) && $(this).scrollTop() < $(window).height()*0.9 - $('.top-nav').height()) {	//Using 90% of the window height because the cover page is 90vh.
+		$('.top-nav').addClass('on-main-header'); 
+   }
+   else { 
+   	$('.top-nav').removeClass('on-main-header'); 
+   }
+});
+
+$(window).scroll(function (event) {
+	var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+		if (st > 42) { $('.top-nav').removeClass('awake'); }
+   } 
+   else {
+		$('.top-nav').addClass('awake');
+   }
+   lastScrollTop = st;
+
+   //If the about page is open, hide it
+   $('.open-about').removeClass('open-about');
+   $('.about-overlay').addClass('hidden');
+
+
+   //Color the header if it's on the main title
+   if (!($('.top-nav').hasClass('project-page')) && st < $(window).height()*0.9 - $('.top-nav').height()) {	//Using 90% of the window height because the cover page is 90vh.
+		$('.top-nav').addClass('on-main-header'); 
+   }
+   else { 
+   	$('.top-nav').removeClass('on-main-header'); 
+   }   
 });
